79a2f09b2ab96ffcca25cc32b1769212e8b65adb
Justin Clarke <justinclarke241@gmail.com>
2021-03-20 12:58:44 +0530

    data changed

--- data.py
+++ data.py
@@ -1,13 +1,20 @@
-question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}]
+question_data = {
+    "response_code":0,
+    "results":[
+        {"category":"Entertainment: Music",
+         "type":"boolean",
+         "difficulty":"medium",
+         "question":"Scottish producer Calvin Harris is from the town of Dumfries, Scotland.",
+         "correct_answer":"True",
+         "incorrect_answers":["False"]},
+        
+        {"category":"General Knowledge","type":"boolean","difficulty":"medium","question":"Furby was released in 1998.","correct_answer":"True","incorrect_answers":["False"]},
+        {"category":"Science: Computers","type":"boolean","difficulty":"hard","question":"The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.","correct_answer":"False","incorrect_answers":["True"]},
+        {"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In Terraria, you can craft the Cell Phone pre-hardmode.","correct_answer":"True","incorrect_answers":["False"]},
+        {"category":"Entertainment: Film","type":"boolean","difficulty":"easy","question":"&quot;Minions&quot; was released on the June 10th, 2015.","correct_answer":"False","incorrect_answers":["True"]},
+        {"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"In &quot;Super Mario World&quot;, the rhino mini-boss, Reznor, is named after the lead singer of the band &quot;Nine Inch Nails&quot;.","correct_answer":"True","incorrect_answers":["False"]},
+        {"category":"Politics","type":"boolean","difficulty":"medium","question":"Helen Clark was the 37th Prime Minister of Australia.","correct_answer":"False","incorrect_answers":["True"]},
+        {"category":"Science & Nature","type":"boolean","difficulty":"easy","question":"Celiac Disease is a disease that effects the heart, causing those effected to be unable to eat meat.","correct_answer":"False","incorrect_answers":["True"]},
+        {"category":"History","type":"boolean","difficulty":"medium","question":"Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.","correct_answer":"True","incorrect_answers":["False"]},
+        {"category":"Entertainment: Japanese Anime & Manga","type":"boolean","difficulty":"medium","question":"In Full Metal Panic!, Whispered are those who are capable of creating Black Technology.","correct_answer":"True","incorrect_answers":["False"]}
+        ]}
