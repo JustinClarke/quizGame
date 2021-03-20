17b756fe11f0d1c061d07f6d00a280046f44acee
Justin Clarke <justinclarke241@gmail.com>
2021-03-20 13:19:47 +0530

    bug fixes

--- data.py
+++ data.py
@@ -1,13 +1,10 @@
-question_data = {
-    "response_code":0,
-    "results":[
+question_data = [
         {"category":"Entertainment: Music",
          "type":"boolean",
          "difficulty":"medium",
          "question":"Scottish producer Calvin Harris is from the town of Dumfries, Scotland.",
          "correct_answer":"True",
          "incorrect_answers":["False"]},
-        
         {"category":"General Knowledge","type":"boolean","difficulty":"medium","question":"Furby was released in 1998.","correct_answer":"True","incorrect_answers":["False"]},
         {"category":"Science: Computers","type":"boolean","difficulty":"hard","question":"The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.","correct_answer":"False","incorrect_answers":["True"]},
         {"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"In Terraria, you can craft the Cell Phone pre-hardmode.","correct_answer":"True","incorrect_answers":["False"]},
@@ -17,4 +14,4 @@ question_data = {
         {"category":"Science & Nature","type":"boolean","difficulty":"easy","question":"Celiac Disease is a disease that effects the heart, causing those effected to be unable to eat meat.","correct_answer":"False","incorrect_answers":["True"]},
         {"category":"History","type":"boolean","difficulty":"medium","question":"Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.","correct_answer":"True","incorrect_answers":["False"]},
         {"category":"Entertainment: Japanese Anime & Manga","type":"boolean","difficulty":"medium","question":"In Full Metal Panic!, Whispered are those who are capable of creating Black Technology.","correct_answer":"True","incorrect_answers":["False"]}
-        ]}
+    ]
